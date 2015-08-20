341fa8093bc17889f361827fd40f4a3c88bb1cec
hophiaSourceTree <zyfhophia@163.com>
2015-08-20 15:12:27 +0800

    这是第一次提交

---
+++ hophiaRepository/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hophiaRepository
+//
+//  Created by 赵玉凤 on 15/8/20.
+//  Copyright (c) 2015年 赵玉凤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
